620f27dc7a8ae6ba6678a3472c97a9389e0ba9a7
tprashan <tprashan@thoughtworks.com>
2016-06-09 23:26:24 +0530

    #NFA_epsilon | handel multiple epsilon and passed tests.

--- DFA_Automata/src/nfa_generator.js
+++ DFA_Automata/src/nfa_generator.js
@@ -6,36 +6,45 @@ var varifyStringOfLanguage = function(stringOfLanguage){
 	return /^[0-1]+$/.test(stringOfLanguage);
 }
 
-var getAllEpsilonStates = function(tuples,state,currentAlphabet){
-	return (tuples.transitionFunction[state]['e'].map(function(epsilonState){
-			return tuples.transitionFunction[epsilonState][currentAlphabet] || epsilonState;
-		}));
+var getAllEpsilonStates = function(transitionFunction,states) {
+	if(!Array.isArray(states)) states = _.filter([states],Boolean);
+	return states.reduce(function(epsilonStates, state) {
+		var  newEpsilonStates = transitionFunction[state]['e'];
+		epsilonStates = _.filter(epsilonStates.concat(newEpsilonStates),Boolean);
+		return epsilonStates.concat(getAllEpsilonStates(transitionFunction,newEpsilonStates));
+	}, []);
 }
 
-var getNextStateOfAlphabet = function(tuples,state,currentAlphabet){
-	return tuples.transitionFunction[state][currentAlphabet];
+
+var getNextStateOfAlphabet = function(transitionFunction,state,currentAlphabet){
+	return transitionFunction[state][currentAlphabet] || [];
 }
 
-var getAllStateWithEpsilon = function(tuples,state,currentAlphabet){
-	var resultEpsilonStates=[];
-	if(tuples.transitionFunction[state]['e'])
-		resultEpsilonStates.push(getAllEpsilonStates(tuples,state,currentAlphabet));
-
-	if(tuples.transitionFunction[state][currentAlphabet])
-		resultEpsilonStates.push(getNextStateOfAlphabet(tuples,state,currentAlphabet));
-	return resultEpsilonStates;
+var getAllStateWithEpsilon = function(transitionFunction,state,currentAlphabet){
+	var validStates=[];var validElipsonStates=[];
+	validStates.push(getNextStateOfAlphabet(transitionFunction,state,currentAlphabet));	
+	transitionFunction[state]['e'] && validElipsonStates.push(getAllEpsilonStates(transitionFunction,state));
+	return validStates.concat(_.flattenDeep(validElipsonStates).map(function(state1){
+		return getNextStateOfAlphabet(transitionFunction,state1,currentAlphabet);
+	}));	
 }
 
 
 nfaGenerator.generateNFA = function (tuples) {
 	return function(stringOfLanguage){
-		if(!varifyStringOfLanguage(stringOfLanguage)) return false;
 		var resultState = stringOfLanguage.split("").reduce(function(currentStates,currentAlphabet){
 			return _.flattenDeep(currentStates.map(function(state){
-				return getAllStateWithEpsilon(tuples,state,currentAlphabet);
+				return _.flattenDeep(getAllStateWithEpsilon(tuples.transitionFunction,state,currentAlphabet));
 			}));
-		},tuples.initialState)
-		return _.intersection(tuples.finalState,resultState).length > 0;
+		},tuples.initialState);
+
+		var finalStates = _.flattenDeep(resultState.map(function(state){
+			if(tuples.transitionFunction[state]['e'])
+				return getAllEpsilonStates(tuples.transitionFunction,state);
+			return resultState;
+		}))
+
+		return _.intersection(tuples.finalState,finalStates).length > 0;
 	}
 }
 
